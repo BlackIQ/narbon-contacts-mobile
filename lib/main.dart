d60d8a09ac9edb6983a2c6baedce9eed765643a2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-30 21:04:10 +0330

    All in one

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:narboncontacts/app/screens/home.dart';
-
+import 'dart:io';
 void main() {
   runApp(const MyApp());
 }
@@ -10,12 +10,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Narbon contacts',
       theme: ThemeData(
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const WebView(
+      initialUrl: 'http://office.narbon.ir:4488/contacts',
     );
   }
 }
